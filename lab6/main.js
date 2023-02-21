2921f16332f8f252f868889964add92b5ec36872
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-02-21 12:18:31 -0600

    Added informative text

--- lab6/main.js
+++ lab6/main.js
@@ -25,7 +25,20 @@ password.addEventListener("keyup", () => {
 });
 
 function validatePassword() {
-  const reqCharacters = ["!", "#", "$", "%", "&"];
+  const reqCharacters = [
+    "!",
+    "#",
+    "$",
+    "%",
+    "&",
+    ",",
+    ".",
+    "/",
+    "=",
+    "+",
+    "?",
+    "_",
+  ];
   const reqNums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
   let containsSpecChar = false;
